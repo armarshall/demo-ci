bcff87b84e6d332cfdf2e059852ca23178287009
armarshall <armarsh1023@gmail.com>
2025-11-06 10:06:41 -0500

    Changing function addition

--- src/main/java/br/ufmg/dcc/SimpleCalculator.java
+++ src/main/java/br/ufmg/dcc/SimpleCalculator.java
@@ -6,7 +6,7 @@ public class SimpleCalculator {
 
     // Hello World!
   public int addition(int x, int y) {
-    return x + y;
+    return x + y + 1;
   }
 
   public int subtraction(int x, int y) {
